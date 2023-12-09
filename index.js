997c59cc1ab67738339da5f8bec8765361cf88aa
ebanchik <ebanchik@gmail.com>
2023-12-09 11:41:41 -0500

    second web scraper

---
+++ index.js
@@ -0,0 +1,32 @@
+const puppeteer = require('puppeteer');
+// puppeteer.launch(headless: "new")
+
+async function run() {
+  const browser = await puppeteer.launch({
+    headless: "new",
+  });
+  const page = await browser.newPage();
+  await page.goto('https://www.mybackrlyhurts.com/about-4/');
+
+  // await page.screenshot({ path: 'example.png', fullPage: true});
+  // await page.pdf({ path: 'example.pdf', format: 'A4'});
+
+  // const html = await page.content();
+  // console.log(html);
+  
+  // const title = await page.evaluate(() => document.title);
+  // console.log(title);
+
+  // const text = await page.evaluate(() => document.body.innerText);  
+  // console.log(text);
+
+  // const links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), (e) => e.href));
+  // console.log(links)
+
+  const links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), (e) => e.href));
+  console.log(links)
+
+  await browser.close();
+}
+
+run();
