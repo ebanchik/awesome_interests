f7d22bb57a780ec54d750c48f7573d5f10de609d
ebanchik <ebanchik@gmail.com>
2023-12-11 16:53:37 -0500

    adding file saving

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const puppeteer = require('puppeteer');
 // puppeteer.launch(headless: "new")
 
@@ -11,20 +12,22 @@ async function run() {
   // await page.screenshot({ path: 'example.png', fullPage: true});
   // await page.pdf({ path: 'example.pdf', format: 'A4'});
 
-  // const html = await page.content();
-  // console.log(html);
+  const html = await page.content();
+  console.log(html);
   
-  // const title = await page.evaluate(() => document.title);
-  // console.log(title);
+  const title = await page.evaluate(() => document.title);
+  console.log(title);
 
-  // const text = await page.evaluate(() => document.body.innerText);  
-  // console.log(text);
+  const text = await page.evaluate(() => document.body.innerText);  
+  console.log(text);
+
+  const links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), (e) => e.href));
+  console.log(links)
 
   // const links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), (e) => e.href));
   // console.log(links)
 
-  const links = await page.evaluate(() => Array.from(document.querySelectorAll('a'), (e) => e.href));
-  console.log(links)
+  fs.writeFileSync('home_text.txt', text);
 
   await browser.close();
 }
